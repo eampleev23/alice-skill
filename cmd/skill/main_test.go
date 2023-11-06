079a9e7ea4060509c5f7bd9f91317d2df05f1b8c
Евгений Амплеев <ampleeve@192.168.1.4>
2023-11-06 11:05:36 +0300

    add tests

---
+++ cmd/skill/main_test.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestWebhook(t *testing.T) {
+	// описываем ожидаемое тело ответа при успешном запросе
+	successBody := `{"response":{"text":"Извините, я пока ничего не умею"},"version":"1.0"}`
+
+	// описываем набор данных: метод запроса, ожидаемый код ответа, ожидаемое тело
+	testCases := []struct {
+		method       string
+		expectedCode int
+		expectedBody string
+	}{
+		{method: http.MethodGet, expectedCode: http.StatusMethodNotAllowed, expectedBody: ""},
+		{method: http.MethodPut, expectedCode: http.StatusMethodNotAllowed, expectedBody: ""},
+		{method: http.MethodDelete, expectedCode: http.StatusMethodNotAllowed, expectedBody: ""},
+		{method: http.MethodPost, expectedCode: http.StatusOK, expectedBody: successBody},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.method, func(t *testing.T) {
+			r := httptest.NewRequest(tc.method, "/", nil)
+			w := httptest.NewRecorder()
+
+			// вызовем хэндлер как обычную функцию без запуска сервера
+			webhook(w, r)
+
+			assert.Equal(t, tc.expectedCode, w.Code, "Код ответа не совпадает с ожидаемым")
+
+			// проверим корректность полученного тела ответа если мы его ожидаем
+			if tc.expectedBody != "" {
+				// assert.JSONq помогает сравнить 2 json строки
+				assert.JSONEq(t, tc.expectedBody, w.Body.String(), "Тело ответа не совпадает с ожидаемым")
+			}
+		})
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
-module github.com/eampleev23/alice-skill
+module github.com/eampleev23/alice-skill.git
 
-go 1.18
+go 1.21.3
+
+require github.com/stretchr/testify v1.8.4
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
