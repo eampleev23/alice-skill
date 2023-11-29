61c6850aa3866b8653c3d858a8abe6c436fc706c
Евгений Амплеев <ampleeve@192.168.1.4>
2023-11-29 18:27:08 +0300

    add response by serialize json

--- cmd/skill/main.go
+++ cmd/skill/main.go
@@ -2,7 +2,9 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/eampleev23/alice-skill.git/internal/logger"
+	"github.com/eampleev23/alice-skill.git/internal/models"
 	"go.uber.org/zap"
 	"log"
 	"net/http"
@@ -33,19 +35,40 @@ func run() error {
 func webhook(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		// разрешаем только POST-запросы
+		logger.Log.Debug("got request with bad method", zap.String("method", r.Method))
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
 
-	// установим правильный заголовок для типа данных
+	// десериализуем запрос в структуру модели
+	logger.Log.Debug("decoding request")
+	var req models.Request
+	dec := json.NewDecoder(r.Body)
+	if err := dec.Decode(&req); err != nil {
+		logger.Log.Debug("cannot decode request JSON body", zap.Error(err))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	// Проверяем что пришел запрос понятного типа
+	if req.Request.Type != models.TypeSimpleUtterance {
+		logger.Log.Debug("unsupported request type", zap.String("type", req.Request.Type))
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		return
+	}
+	// Заполням модель ответа
+	resp := models.Response{
+		Response: models.ResponsePayload{
+			Text: "Извините, я пока ничего не умею",
+		},
+		Version: "1.0",
+	}
 	w.Header().Set("Content-Type", "application/json")
-	// пока установим ответ-заглушку, без проверки ошибок
-	_, _ = w.Write([]byte(`
-      {
-        "response": {
-          "text": "Извините, я пока ничего не умею"
-        },
-        "version": "1.0"
-      }
-    `))
+
+	// Сериализуем ответ сервера
+	enc := json.NewEncoder(w)
+	if err := enc.Encode(resp); err != nil {
+		logger.Log.Debug("error encoding response", zap.Error(err))
+		return
+	}
+	logger.Log.Debug("Sending HTTP 200 response")
 }
--- cmd/skill/main_test.go
+++ cmd/skill/main_test.go
@@ -1,55 +1,89 @@
 package main
 
 import (
-	"fmt"
-	"github.com/go-resty/resty/v2"
-	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/go-resty/resty/v2"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestWebhook(t *testing.T) {
-	// тип http.HandlerFunc реализует интерфейс http.Handler
-	// это поможет передать хэндлер тестовому серверу
 	handler := http.HandlerFunc(webhook)
-	// запускаем тестовый сервер, будет выбран первый свободный порт
 	srv := httptest.NewServer(handler)
-	// останавливаем сервер после завершения теста
 	defer srv.Close()
 
-	// ожидаемое тело ответа при успешном запросе
 	successBody := `{
         "response": {
             "text": "Извините, я пока ничего не умею"
         },
         "version": "1.0"
     }`
-	// Описываем набор данных:метод запроса, ожидаемый код ответа, ожидаемое тело
+
 	testCases := []struct {
+		name         string // добавляем название тестов
 		method       string
+		body         string // добавляем тело запроса в табличные тесты
 		expectedCode int
 		expectedBody string
 	}{
-		{method: http.MethodGet, expectedCode: http.StatusMethodNotAllowed, expectedBody: ""},
-		{method: http.MethodPut, expectedCode: http.StatusMethodNotAllowed, expectedBody: ""},
-		{method: http.MethodDelete, expectedCode: http.StatusMethodNotAllowed, expectedBody: ""},
-		{method: http.MethodPost, expectedCode: http.StatusOK, expectedBody: successBody},
+		{
+			name:         "method_get",
+			method:       http.MethodGet,
+			expectedCode: http.StatusMethodNotAllowed,
+			expectedBody: "",
+		},
+		{
+			name:         "method_put",
+			method:       http.MethodPut,
+			expectedCode: http.StatusMethodNotAllowed,
+			expectedBody: "",
+		},
+		{
+			name:         "method_delete",
+			method:       http.MethodDelete,
+			expectedCode: http.StatusMethodNotAllowed,
+			expectedBody: "",
+		},
+		{
+			name:         "method_post_without_body",
+			method:       http.MethodPost,
+			expectedCode: http.StatusInternalServerError,
+			expectedBody: "",
+		},
+		{
+			name:         "method_post_unsupported_type",
+			method:       http.MethodPost,
+			body:         `{"request": {"type": "idunno", "command": "do something"}, "version": "1.0"}`,
+			expectedCode: http.StatusUnprocessableEntity,
+			expectedBody: "",
+		},
+		{
+			name:         "method_post_success",
+			method:       http.MethodPost,
+			body:         `{"request": {"type": "SimpleUtterance", "command": "sudo do something"}, "version": "1.0"}`,
+			expectedCode: http.StatusOK,
+			expectedBody: successBody,
+		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.method, func(t *testing.T) {
-			// делаем запрос с помощью библиотеки resty к адресу запущенного сервера,
-			// который хранится в поле url соответствующей структуры
 			req := resty.New().R()
 			req.Method = tc.method
 			req.URL = srv.URL
-			// посмотрим как выглядит адрес сервера
-			fmt.Println("srv.URL ", srv.URL)
+
+			if len(tc.body) > 0 {
+				req.SetHeader("Content-Type", "application/json")
+				req.SetBody(tc.body)
+			}
+
 			resp, err := req.Send()
-			assert.NoError(t, err, "Error making HTTP request ")
+			assert.NoError(t, err, "error making HTTP request")
+
 			assert.Equal(t, tc.expectedCode, resp.StatusCode(), "Response code didn't match expected")
-			// Проверяем корректность полученного ответа если мы его ожидаем
+			// проверяем корректность полученного тела ответа, если мы его ожидаем
 			if tc.expectedBody != "" {
 				assert.JSONEq(t, tc.expectedBody, string(resp.Body()))
 			}
