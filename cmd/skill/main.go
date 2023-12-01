4f7a5aef520ba2137c163beb44d2efa768e74c14
Евгений Амплеев <ampleeve@MacBook-Pro-Evgenij.local>
2023-12-01 07:27:51 +0300

    add gzip with tests

--- cmd/skill/main.go
+++ cmd/skill/main.go
@@ -8,6 +8,7 @@ import (
 	"go.uber.org/zap"
 	"log"
 	"net/http"
+	"strings"
 )
 
 // функция main вызывается автоматически при запуске приложения
@@ -18,17 +19,54 @@ func main() {
 		log.Fatal(err)
 	}
 }
+func gzipMiddleware(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// по умолчанию устанавливаем оригинальный http.ResponseWriter как тот,
+		// который будем передавать следующей функции
+		ow := w
 
-// функция run будет полезна при инициализации зависимостей сервера перед запуском
+		// проверяем, что клиент умеет получать от сервера сжатые данные в формате gzip
+		acceptEncoding := r.Header.Get("Accept-Encoding")
+		supportsGzip := strings.Contains(acceptEncoding, "gzip")
+		if supportsGzip {
+			// оборачиваем оригинальный http.ResponseWriter новым, с поддержкой сжатия
+			cw := newCompressWriter(w)
+			// меняем оригинальный http.ResponseWriter на новый
+			ow = cw
+			// не забываем отправить клиенту все сжатые данные после завершения middleware
+			defer cw.Close()
+		}
+
+		// проверяем, что клиент отправил серверу сжатые данные в формате gzip
+		contentEncoding := r.Header.Get("Content-Encoding")
+		sendsGzip := strings.Contains(contentEncoding, "gzip")
+		if sendsGzip {
+			// оборачиваем тело запроса в io.Reader с поддержкой декомпрессии
+			cr, err := newCompressReader(r.Body)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			// меняем тело запроса на новое
+			r.Body = cr
+			defer cr.Close()
+		}
+
+		// передаём управление хендлеру
+		h.ServeHTTP(ow, r)
+	}
+}
+
+// ...
+
 func run() error {
-
 	if err := logger.Initialize(flagLogLevel); err != nil {
 		return err
 	}
 
 	logger.Log.Info("Running server", zap.String("address", flagRunAddr))
-
-	return http.ListenAndServe(flagRunAddr, logger.RequestLogger(webhook))
+	// оборачиваем хендлер webhook в middleware с логированием и поддержкой gzip
+	return http.ListenAndServe(flagRunAddr, logger.RequestLogger(gzipMiddleware(webhook)))
 }
 
 // функция webhook — обработчик HTTP-запроса
